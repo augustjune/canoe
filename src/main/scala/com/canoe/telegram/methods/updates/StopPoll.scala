f84d8025b865cdccacfb09b906616c3111db2d13
Yura Slinkin <jurij.jurich@gmail.com>
2019-06-09 19:09:35 +0200

    Polling methods with corresponding models

---
+++ src/main/scala/com/canoe/telegram/methods/updates/StopPoll.scala
@@ -0,0 +1,18 @@
+package com.canoe.telegram.methods.updates
+
+import com.canoe.telegram.methods.JsonRequest
+import com.canoe.telegram.models.{ChatId, Poll, ReplyMarkup}
+
+
+/**
+  * Use this method to stop a poll which was sent by the bot.
+  * On success, the stopped Poll with the final results is returned.
+  *
+  * @param chatId       Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+  * @param messageId    Identifier of the original message with the poll
+  * @param replyMarkup  A JSON-serialized object for a new message inline keyboard.
+  */
+case class StopPoll(chatId      : ChatId,
+                    messageId   : Option[Int] = None,
+                    replyMarkup : Option[ReplyMarkup] = None
+                   ) extends JsonRequest[Poll]

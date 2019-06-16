69257e12a63dd978c20ba033aca22864d4e9d3e9
Yura Slinkin <jurij.jurich@gmail.com>
2019-06-16 12:45:57 +0200

    Editing for message api

--- src/main/scala/com/canoe/telegram/api/models/MessageApi.scala
+++ src/main/scala/com/canoe/telegram/api/models/MessageApi.scala
@@ -3,7 +3,7 @@ package com.canoe.telegram.api.models
 import com.canoe.telegram.clients.RequestHandler
 import com.canoe.telegram.methods.chats.StopPoll
 import com.canoe.telegram.methods.messages._
-import com.canoe.telegram.models.{Chat, Message, Poll, ReplyMarkup}
+import com.canoe.telegram.models.{Chat, InlineKeyboardMarkup, Message, Poll, ReplyMarkup}
 
 final class MessageApi[F[_]](message: Message)
                             (implicit client: RequestHandler[F]) {
@@ -16,6 +16,15 @@ final class MessageApi[F[_]](message: Message)
 
   def reply(text: String): F[Message] =
     client.execute(SendMessage(chatId, text, replyToMessageId = Option(messageId)))
+
+  def editText(text: String): F[Either[Boolean, Message]] =
+    client.execute(EditMessageText(Some(chatId), Some(messageId), text = text))
+
+  def editReplyMarkup(keyboard: Option[InlineKeyboardMarkup]): F[Either[Boolean, Message]] =
+    client.execute(EditMessageReplyMarkup(Some(chatId), Some(messageId), replyMarkup = keyboard))
+
+  def editCaption(caption: Option[String]): F[Either[Boolean, Message]] =
+    client.execute(EditMessageCaption(Some(chatId), Some(messageId), caption = caption))
 
   // ToDo - handle case, when the message doesn't correspond to the poll
   def stopPoll(markup: Option[ReplyMarkup] = None): F[Poll] =

59132dcc5e4910e7d1f6e438c8727102a00f4541
Yura Slinkin <jurij.jurich@gmail.com>
2019-06-28 00:01:04 +0200

    Moved additional methods to Expect (recoverWith, or). Provided builder methods

--- src/main/scala/com/canoe/telegram/scenarios/Simple.scala
+++ src/main/scala/com/canoe/telegram/scenarios/Simple.scala
@@ -1,9 +1,9 @@
 package com.canoe.telegram.scenarios
 
-import cats.effect.{Concurrent, ExitCode, IO, IOApp, Sync}
+import cats.{Applicative, Id}
+import cats.effect.{ExitCode, IO, IOApp, Sync}
 import cats.syntax.all._
 import fs2.Stream
-import fs2.concurrent.Topic
 
 import scala.io.StdIn
 
@@ -18,12 +18,7 @@ object Simple extends IOApp {
     s
   }
 
-  def evalLog[F[_] : Concurrent, A](id: String)(s: A): F[A] = Concurrent[F].delay {
-    println(s"$id: $s")
-    s
-  }
-
-  implicit class MessageScenario[F[_] : Concurrent, A <: Message](val scenario: Scenario[F, A]) {
+  implicit class MessageScenario[F[_], A <: Message](val scenario: Scenario[F, A]) {
     def withinChat: Scenario[F, A] = new Scenario[F, A] {
       def fulfill(messages: Stream[F, Message]): Stream[F, (A, Stream[F, Message])] =
         scenario.fulfill(messages).map { case (a, rest) => a -> rest.filter(chat(_) == chat(a)) }
@@ -34,37 +29,39 @@ object Simple extends IOApp {
     private def fullPredicate[A](pf: PartialFunction[A, Boolean]): A => Boolean =
       pf.applyOrElse(_, (_: A) => false)
 
-    //    def receive[F[_]](pf: PartialFunction[Message, Boolean]): Scenario[F, Message] =
-    //      Receive(fullPredicate(pf))
-    //
-    //    def receive[F[_]](p: Message => Boolean): Scenario[F, Message] =
-    //      Receive(p)
-    //
-    //    def expect[F[_]](pf: PartialFunction[Message, Boolean]): Scenario[F, Message] =
-    //      Expect(fullPredicate(pf))
-    //
-    //    def expect[F[_]](p: Message => Boolean): Scenario[F, Message] =
-    //      Expect(p)
-    //
-    //    def execute[F[_], A](fa: F[A]): Scenario[F, A] =
-    //      Action(fa)
-    //
-    //    def pure[F[_] : Applicative, A](a: A): Scenario[F, A] =
-    //      execute(a.pure[F])
-    //
-    //    def pure[A](a: A): Scenario[Id, A] =
-    //      execute[Id, A](a)
-    //
-    //    def unit[F[_] : Applicative]: Scenario[F, Unit] =
-    //      pure(())
+    // ToDo - make builder methods return common abstraction i.e. Scenario
+    //  (currently undesirable because of Expect methods, such as recover)
+    def receive[F[_]](pf: PartialFunction[Message, Boolean]): Receive[F] =
+      Receive(fullPredicate(pf))
 
-    def end[F[_] : Concurrent, A]: Scenario[F, A] =
+    def receive[F[_]](p: Message => Boolean): Receive[F] =
+      Receive(p)
+
+    def expect[F[_]](pf: PartialFunction[Message, Boolean]): Expect[F] =
+      Expect(fullPredicate(pf))
+
+    def expect[F[_]](p: Message => Boolean): Expect[F] =
+      Expect(p)
+
+    def execute[F[_], A](fa: F[A]): Action[F, A] =
+      Action(fa)
+
+    def pure[F[_] : Applicative, A](a: A): Scenario[F, A] =
+      execute(a.pure[F])
+
+    def pure[A](a: A): Scenario[Id, A] =
+      execute[Id, A](a)
+
+    def unit[F[_] : Applicative]: Scenario[F, Unit] =
+      pure(())
+
+    def end[F[_], A]: Scenario[F, A] =
       new Scenario[F, A] {
         def fulfill(messages: Stream[F, Message]): Stream[F, (A, Stream[F, Message])] = Stream.empty
       }
   }
 
-  sealed abstract class Scenario[F[_] : Concurrent, A] extends (Stream[F, Message] => Stream[F, A]) {
+  sealed abstract class Scenario[F[_], A] extends (Stream[F, Message] => Stream[F, A]) {
     self =>
 
     def apply(messages: Stream[F, Message]): Stream[F, A] =
@@ -77,23 +74,6 @@ object Simple extends IOApp {
         self.fulfill(messages).map { case (a, rest) => (fn(a), rest) }
     }
 
-    def or[B](other: => Scenario[F, B]): Scenario[F, Either[A, B]] =
-      new Scenario[F, Either[A, B]] {
-        def fulfill(messages: Stream[F, Message]): Stream[F, (Either[A, B], Stream[F, Message])] = {
-          (self.fulfill(messages).map { case (a, rest) => Left(a) -> rest } ++
-            other.fulfill(messages).map { case (b, rest) => Right(b) -> rest }).head
-        }
-      }
-
-    def recoverWith(fn: Message => Scenario[F, A]): Scenario[F, A] =
-      new Scenario[F, A] {
-        def fulfill(messages: Stream[F, Message]): Stream[F, (A, Stream[F, Message])] = {
-          val primary = self.fulfill(messages)
-          val secondary = messages.head.map(fn).flatMap(sc => sc.fulfill(messages))
-          (primary ++ secondary).head
-        }
-      }
-
     def flatMap[B](fn: A => Scenario[F, B]): Scenario[F, B] = {
       new Scenario[F, B] {
         def fulfill(messages: Stream[F, Message]): Stream[F, (B, Stream[F, Message])] =
@@ -102,14 +82,14 @@ object Simple extends IOApp {
     }
   }
 
-  final case class Receive[F[_] : Concurrent](p: Message => Boolean) extends Scenario[F, Message] {
-    self =>
+  final case class Receive[F[_]](p: Message => Boolean) extends Scenario[F, Message] {
     def fulfill(messages: Stream[F, Message]): Stream[F, (Message, Stream[F, Message])] =
       messages.filter(p).map(_ -> messages)
   }
 
-  final case class Expect[F[_] : Concurrent](p: Message => Boolean) extends Scenario[F, Message] {
+  final case class Expect[F[_]](p: Message => Boolean) extends Scenario[F, Message] {
     self =>
+
     def fulfill(messages: Stream[F, Message]): Stream[F, (Message, Stream[F, Message])] =
       messages.head.filter(p).map(_ -> messages)
 
@@ -120,9 +100,27 @@ object Simple extends IOApp {
           else Stream.eval(fn(m)) *> self.fulfill(messages)
         }
     }
+
+    def recoverWith(fn: Message => Scenario[F, Message]): Scenario[F, Message] =
+      new Scenario[F, Message] {
+        def fulfill(messages: Stream[F, Message]): Stream[F, (Message, Stream[F, Message])] =
+          messages.head.flatMap { m =>
+            if (p(m)) self.fulfill(Stream(m) ++ messages)
+            else fn(m).fulfill(messages)
+          }
+      }
+
+    def or[B](other: => Scenario[F, B]): Scenario[F, Either[Message, B]] =
+      new Scenario[F, Either[Message, B]] {
+        def fulfill(messages: Stream[F, Message]): Stream[F, (Either[Message, B], Stream[F, Message])] =
+          messages.head.flatMap { m =>
+            if (p(m)) self.fulfill(Stream(m) ++ messages).map { case (a, rest) => Left(a) -> rest }
+            else other.fulfill(Stream(m) ++ messages).map { case (a, rest) => Right(a) -> rest }
+          }
+      }
   }
 
-  final case class Action[F[_] : Concurrent, A](fa: F[A]) extends Scenario[F, A] {
+  final case class Action[F[_], A](fa: F[A]) extends Scenario[F, A] {
     def fulfill(messages: Stream[F, Message]): Stream[F, (A, Stream[F, Message])] =
       Stream.eval(fa).map(_ -> messages)
   }
@@ -130,11 +128,8 @@ object Simple extends IOApp {
   val greetings: Scenario[IO, Unit] =
     for {
       _ <- Receive[IO](_.startsWith("start"))
-
       _ <- Action(IO(println("ok, let's go")))
-
       _ <- Expect[IO](_ == "1").recover(_ => IO(println("Please provide 1")))
-
       _ <- Action(IO(println("Done.")))
     } yield ()
 

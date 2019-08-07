d208f9005bbb3ca6b8dbe7320a61d6713c9ea87c
augustjune <jurij.jurich@gmail.com>
2019-08-07 21:25:40 +0200

    Simple example scenario implementation

--- src/main/scala/com/canoe/telegram/scenarios/Simple.scala
+++ src/main/scala/com/canoe/telegram/scenarios/Simple.scala
@@ -1,30 +1,46 @@
 package com.canoe.telegram.scenarios
 
+import cats.effect.concurrent.Ref
 import cats.{Applicative, Id}
 import cats.effect.{Concurrent, ExitCode, IO, IOApp, Sync}
 import cats.syntax.all._
+import fs2.concurrent.Broadcast
 import fs2.{Pipe, Pull, Stream}
 
 import scala.io.StdIn
+import scala.util.{Failure, Random, Success, Try}
 
 object Simple extends IOApp {
 
   type Message = String
 
-  def chat(m: Message): String = m.take(1)
+  def chat(m: Message): String = m.takeWhile(_ != '.')
 
-  def log[F[_] : Sync, A](id: String)(s: A): F[Unit] = Sync[F].delay {
-    println(s"$id: $s")
-  }
+  def log[F[_] : Sync, A](id: String)(s: A): F[Unit] =
+    Sync[F].delay(println(s"$id: $s"))
 
-  implicit class MessageScenario[F[_], A <: Message](val scenario: Scenario[F, A]) {
-    def withinChat: Scenario[F, A] = new Scenario[F, A] {
-      def fulfill(messages: Stream[F, Message]): Stream[F, (A, Stream[F, Message])] =
-        scenario.fulfill(messages).map { case (a, rest) => a -> rest.filter(chat(_) == chat(a)) }
+  sealed abstract class ChatScenario[F[_], A] extends Pipe[F, Message, A] {
+    self =>
+
+    def apply(messages: Stream[F, Message]): Stream[F, A] =
+      fulfill(messages).map(_._1)
+
+    def fulfill(messages: Stream[F, Message]): Stream[F, (A, Stream[F, Message])]
+
+    def map[B](fn: A => B): ChatScenario[F, B] = new ChatScenario[F, B] {
+      def fulfill(messages: Stream[F, Message]): Stream[F, (B, Stream[F, Message])] =
+        self.fulfill(messages).map { case (a, rest) => (fn(a), rest) }
+    }
+
+    def flatMap[B](fn: A => ChatScenario[F, B]): ChatScenario[F, B] = {
+      new ChatScenario[F, B] {
+        def fulfill(messages: Stream[F, Message]): Stream[F, (B, Stream[F, Message])] =
+          self.fulfill(messages).flatMap { case (a, rest) => fn(a).fulfill(rest) }
+      }
     }
   }
 
-  object Scenario {
+  object ChatScenario {
     private def fullPredicate[A](pf: PartialFunction[A, Boolean]): A => Boolean =
       pf.applyOrElse(_, (_: A) => false)
 
@@ -33,73 +49,42 @@ object Simple extends IOApp {
     //    def receive[F[_]](pf: PartialFunction[Message, Boolean]): Receive[F] =
     //      Receive(fullPredicate(pf))
     //
-    //    def receive[F[_]](p: Message => Boolean): Receive[F] =
-    //      Receive(p)
-    //
-    //    def expect[F[_]](pf: PartialFunction[Message, Boolean]): Expect[F] =
-    //      Expect(fullPredicate(pf))
-    //
-    //    def expect[F[_]](p: Message => Boolean): Expect[F] =
-    //      Expect(p)
-    //
-    //    def execute[F[_], A](fa: F[A]): Action[F, A] =
-    //      Action(fa)
-    //
-    //    def pure[F[_] : Applicative, A](a: A): Scenario[F, A] =
-    //      execute(a.pure[F])
-    //
-    //    def pure[A](a: A): Scenario[Id, A] =
-    //      execute[Id, A](a)
-    //
-    //    def unit[F[_] : Applicative]: Scenario[F, Unit] =
-    //      pure(())
-    //
-    //    def end[F[_], A]: Scenario[F, A] =
-    //      new Scenario[F, A] {
-    //        def fulfill(messages: Stream[F, Message]): Stream[F, (A, Stream[F, Message])] = Stream.empty
-    //      }
-  }
+    def receive[F[_]](p: Message => Boolean): Receive[F] =
+      Receive(p)
 
-  sealed abstract class Scenario[F[_], A] extends (Stream[F, Message] => Stream[F, A]) {
-    self =>
+    def expect[F[_]](pf: PartialFunction[Message, Boolean]): Expect[F] =
+      Expect(fullPredicate(pf))
 
-    def apply(messages: Stream[F, Message]): Stream[F, A] =
-      fulfill(messages).map(_._1)
+    def expect[F[_]](p: Message => Boolean): Expect[F] =
+      Expect(p)
 
-    def fulfill(messages: Stream[F, Message]): Stream[F, (A, Stream[F, Message])]
+    def execute[F[_], A](fa: F[A]): Action[F, A] =
+      Action(fa)
 
-    def map[B](fn: A => B): Scenario[F, B] = new Scenario[F, B] {
-      def fulfill(messages: Stream[F, Message]): Stream[F, (B, Stream[F, Message])] =
-        self.fulfill(messages).map { case (a, rest) => (fn(a), rest) }
-    }
+    def pure[F[_] : Applicative, A](a: A): ChatScenario[F, A] =
+      execute(a.pure[F])
 
-    def flatMap[B](fn: A => Scenario[F, B]): Scenario[F, B] = {
-      new Scenario[F, B] {
-        def fulfill(messages: Stream[F, Message]): Stream[F, (B, Stream[F, Message])] =
-          self.fulfill(messages).flatMap { case (a, rest) => fn(a).fulfill(rest) }
+    def pure[A](a: A): ChatScenario[Id, A] =
+      execute[Id, A](a)
+
+    def unit[F[_] : Applicative]: ChatScenario[F, Unit] =
+      pure(())
+
+    def end[F[_], A]: ChatScenario[F, A] =
+      new ChatScenario[F, A] {
+        def fulfill(messages: Stream[F, Message]): Stream[F, (A, Stream[F, Message])] = Stream.empty
       }
-    }
   }
 
-  final case class Receive[F[_] : Concurrent](p: Message => Boolean) extends Scenario[F, Message] {
+  final case class Receive[F[_]](p: Message => Boolean) extends ChatScenario[F, Message] {
     self =>
 
-    def fulfill(messages: Stream[F, Message]): Stream[F, (Message, Stream[F, Message])] = fulfillStream(messages)
+    def fulfill(messages: Stream[F, Message]): Stream[F, (Message, Stream[F, Message])] = {
 
-    def fulfillStream(messages: Stream[F, Message]): Stream[F, (Message, Stream[F, Message])] = {
-      messages
-        .evalTap(log[F, Message]("receive before"))
-        .filter(p)
-        .evalTap(log[F, Message]("receive after"))
-        .map(_ -> messages)
-    }
-
-    def fulfillPull(messages: Stream[F, Message]): Stream[F, (Message, Stream[F, Message])] = {
-      def go(s: Stream[F, Message]): Pull[F, (Message, Stream[F, Message]), Unit] = {
-        s.pull.uncons1.flatMap {
+      def go(input: Stream[F, Message]): Pull[F, (Message, Stream[F, Message]), Unit] = {
+        input.dropWhile(!p(_)).pull.uncons1.flatMap {
           case Some((m, rest)) =>
-            if (p(m)) Pull.output1(m -> rest) >> go(rest)
-            else go(rest)
+            Pull.output1(m -> rest) >> go(rest)
 
           case None => Pull.done
         }
@@ -109,17 +94,15 @@ object Simple extends IOApp {
     }
   }
 
-  final case class Expect[F[_] : Sync](p: Message => Boolean) extends Scenario[F, Message] {
+  final case class Expect[F[_]](p: Message => Boolean) extends ChatScenario[F, Message] {
     self =>
 
     def fulfill(messages: Stream[F, Message]): Stream[F, (Message, Stream[F, Message])] =
       messages.head
-        .evalTap(log[F, Message]("expect before"))
         .filter(p)
-        .evalTap(log[F, Message]("expect after"))
         .map(_ -> messages)
 
-    def recover[B](fn: Message => F[B]): Scenario[F, Message] = new Scenario[F, Message] {
+    def recover[B](fn: Message => F[B]): ChatScenario[F, Message] = new ChatScenario[F, Message] {
       def fulfill(messages: Stream[F, Message]): Stream[F, (Message, Stream[F, Message])] =
         messages.head.flatMap { m =>
           if (p(m)) self.fulfill(messages.cons1(m))
@@ -127,46 +110,111 @@ object Simple extends IOApp {
         }
     }
 
-    def recoverWith(fn: Message => Scenario[F, Message]): Scenario[F, Message] =
-      new Scenario[F, Message] {
+    def recoverWith(fn: Message => ChatScenario[F, Message]): ChatScenario[F, Message] =
+      new ChatScenario[F, Message] {
         def fulfill(messages: Stream[F, Message]): Stream[F, (Message, Stream[F, Message])] =
           messages.head.flatMap { m =>
-            if (p(m)) self.fulfill(Stream(m) ++ messages)
+            if (p(m)) self.fulfill(messages.cons1(m))
             else fn(m).fulfill(messages)
           }
       }
 
-    def or[B](other: => Scenario[F, B]): Scenario[F, Either[Message, B]] =
-      new Scenario[F, Either[Message, B]] {
+    def or[B](other: => ChatScenario[F, B]): ChatScenario[F, Either[Message, B]] =
+      new ChatScenario[F, Either[Message, B]] {
         def fulfill(messages: Stream[F, Message]): Stream[F, (Either[Message, B], Stream[F, Message])] =
           messages.head.flatMap { m =>
-            if (p(m)) self.fulfill(Stream(m) ++ messages).map { case (a, rest) => Left(a) -> rest }
-            else other.fulfill(Stream(m) ++ messages).map { case (a, rest) => Right(a) -> rest }
+            if (p(m)) self.fulfill(messages.cons1(m)).map { case (a, rest) => Left(a) -> rest }
+            else other.fulfill(messages.cons1(m)).map { case (a, rest) => Right(a) -> rest }
           }
       }
   }
 
-  final case class Action[F[_], A](fa: F[A]) extends Scenario[F, A] {
+  final case class Action[F[_], A](fa: F[A]) extends ChatScenario[F, A] {
     def fulfill(messages: Stream[F, Message]): Stream[F, (A, Stream[F, Message])] =
       Stream.eval(fa).map(_ -> messages)
   }
 
-  val greetings: Scenario[IO, Unit] =
+  val start: ChatScenario[IO, Unit] =
     for {
-      m1 <- Receive[IO](_.contains("start")).withinChat
+      m1 <- Receive[IO](_.contains("start"))
       _ <- Action(IO(println(s"ok, let's go ${chat(m1)}")))
       m <- Expect[IO](_.contains("end"))
       _ <- Action(IO(println(s"Done $m")))
     } yield ()
 
+  val greetings: ChatScenario[IO, Unit] =
+    for {
+      m1 <- Receive[IO](_.contains("Hello"))
+      _ <- Action(IO(println(s"${chat(m1)}, how are you doing?")))
+      m2 <- Expect[IO](_.contains("fine")) or Expect[IO](_.contains("bad"))
+      _ <- m2 match {
+        case Left(fine) => Action(IO(println("Oh, I'm so happy for you")))
+        case Right(bad) => Action(IO(println("Oh, I'm sorry. Is there something I can do for you?")))
+      }
+    } yield ()
+
+  def forkThrough[F[_] : Concurrent, A](stream: Stream[F, A], pipes: Pipe[F, A, Any]*): Stream[F, A] =
+    stream.through(Broadcast.through((identity: Pipe[F, A, A]) :: pipes.toList.map(_.andThen(_.drain)): _*))
+
+  def runScenario[F[_] : Concurrent, A](scenario: ChatScenario[F, A], messages: Stream[F, Message]): Stream[F, A] = {
+
+    val filterByFirst: Pipe[F, Message, Message] =
+      _.pull.peek1.flatMap {
+        case Some((m, rest)) => rest.filter(chat(_) == chat(m)).pull.echo
+        case None => Pull.done
+      }.stream
+
+    def go(input: Stream[F, Message], ids: Ref[F, Set[String]]): Pull[F, A, Unit] =
+      input.pull.peek1.flatMap {
+        case Some((m, rest)) =>
+          Pull.eval(ids.get.map(set => set.contains(chat(m)))).flatMap {
+            case true => // contains
+              go(rest.tail, ids)
+
+            case false => // doesn't contain
+              //              Pull.eval(Sync[F].delay(println("Adding new id"))) >>
+              Pull.eval(ids.update(_ + chat(m))) >>
+                go(forkThrough(rest, scenario compose filterByFirst).tail, ids)
+          }
+
+        case None => Pull.done
+      }
+
+    Stream.eval(Ref.of(Set.empty[String])).flatMap(ids => go(messages, ids).stream)
+  }
+
   import scala.concurrent.duration._
 
+  val repeat: ChatScenario[IO, Unit] =
+    for {
+      m <- Expect(_ => true)
+      _ <-
+        if (m.contains("stop")) Action(IO(println("Ok, that's all")))
+        else Action(IO(println(m))).flatMap(_ => repeat)
+    } yield ()
+
+  val mock: ChatScenario[IO, Unit] =
+    for {
+      start <- Receive[IO](_.contains("start"))
+      _ <- Try(chat(start).toInt) match {
+        case Success(i) if i % 100 == 0 => Action(IO(println(s"Starting mocking ${chat(start)}")))
+        case _ => Action(IO.unit)
+      }
+      _ <- repeat
+    } yield ()
+
+  val consoleIn: Stream[IO, Message] = Stream.repeatEval(IO(StdIn.readLine()))
+
+  val n = 2000
+
+  val pre: Stream[IO, Message] =
+    Stream
+      .range(0, n).covary[IO]
+      .map(i => s"$i.start")
+
   def run(args: List[String]): IO[ExitCode] =
-  //    Stream.fromIterator[IO, String](Iterator("1.start", "2.start", "2.end", "1.end", "1.sdadasdad", "1.dasda", "2.asd", "2.sda"))
-    Stream.repeatEval(IO(StdIn.readLine()))
-      .evalTap(log[IO, Message]("pulled"))
-      //    .zipWith(Stream.awakeEvery[IO](1.second))((x,_) => x)
-      .through(greetings)
+  //    consoleIn.through(greetings)
+    runScenario(mock, pre ++ consoleIn.flatMap(s => Stream.repeatEval(IO(Random.nextInt(n))).map(i => s"$i.$s").take(20)))
       .compile.toList.map(l => println(s"#${l.size}"))
       .as(ExitCode.Success)
 

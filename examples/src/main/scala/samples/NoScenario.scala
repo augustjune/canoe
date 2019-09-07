8e76c9fba936b05905be9a3d577d85f2bfd39bc0
augustjune <jurij.jurich@gmail.com>
2019-09-07 15:21:00 +0200

    Usage example without scenario

---
+++ examples/src/main/scala/samples/NoScenario.scala
@@ -0,0 +1,33 @@
+package samples
+
+import canoe.api._
+import canoe.models.InlineQuery
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.syntax.functor._
+
+/**
+  * Example of defining behavior of the bot without using Scenario syntax.
+  *
+  * Each incoming inline query will be answered with applied method.
+  */
+object NoScenario extends IOApp {
+
+  val token: String = "<your telegram token>"
+
+  def run(args: List[String]): IO[ExitCode] =
+    TelegramClient
+      .global[IO](token)
+      .use { implicit client =>
+        Bot
+          .polling[IO]
+          .updates
+          .through(pipes.inlineQueries)
+          .evalTap(answerInlineQuery(_).void)
+          .compile
+          .drain
+      }
+      .as(ExitCode.Success)
+
+  def answerInlineQuery[F[_]: TelegramClient](query: InlineQuery): F[Boolean] =
+    query.answer(results = List())
+}

057baaacce5a65241977175704543c4b97415c32
augustjune <jurij.jurich@gmail.com>
2019-09-12 09:40:22 +0200

    Expect ops unit tests

--- core/src/main/scala/canoe/syntax/PartialFunctionOps.scala
+++ core/src/main/scala/canoe/syntax/PartialFunctionOps.scala
@@ -2,12 +2,17 @@ package canoe.syntax
 
 final class PartialFunctionOps[A, B](private val original: PartialFunction[A, B]) extends AnyVal {
 
+  /**
+    * Appends additional condition for the function to be defined at
+    */
   def when(p: B => Boolean): PartialFunction[A, B] =
     new PartialFunction[A, B] {
       def isDefinedAt(in: A): Boolean =
         original.isDefinedAt(in) && p(original.apply(in))
 
-      def apply(in: A): B = original.apply(in)
+      def apply(in: A): B =
+        if (isDefinedAt(in)) original.apply(in)
+        else throw new MatchError(in)
     }
 
   def map[C](f: B => C): PartialFunction[A, C] = original andThen f
---
+++ core/src/test/scala/canoe/syntax/PartialFunctionOpsSpec.scala
@@ -0,0 +1,22 @@
+package canoe.syntax
+
+import org.scalatest.FunSuite
+
+class PartialFunctionOpsSpec extends FunSuite {
+
+  val evenInt: PartialFunction[Int, Int] = {
+    case i if i % 2 == 0 => i
+  }
+
+  test("#when appends new condition to isDefineAt") {
+    val evenBiggerThanTen = evenInt.when(_ > 10)
+    assert(evenBiggerThanTen.isDefinedAt(12))
+    assert(!evenBiggerThanTen.isDefinedAt(6))
+    assert(!evenBiggerThanTen.isDefinedAt(13))
+  }
+
+  test("#map applies provided function over the result") {
+    val evenString = evenInt.map(_.toString)
+    assert(evenString(6) == 6.toString)
+  }
+}
